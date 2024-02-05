0d195e4f2b31601deb88cc67bcdddb150e012142
Roy <roy@gastom.com.tw>
2024-02-05 17:21:20 +0800

    add Amei img and image animation

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -42,7 +42,9 @@ const textPosition = (index,isWin) => {
     transform: style,
   }
 }
-
+const showAmei = ref(false)
+const catHandReady = ref(0) // 0 原始位置 1 伸出 2 向回收 
+const catHandSet = ref(false)
 const wheel = ref(null)
 onMounted(() => {
   wheel.value = instance.refs.wheel
@@ -55,11 +57,21 @@ const start = () => {
   wheel.value.style.transition = 'transform 5s ease-out';
   wheel.value.style.transform = `rotate(${totalRotation}deg)`;
 
+  setTimeout(() => {
+    showAmei.value = true
+  }, 2500);
+  setTimeout(() => {
+    catHandReady.value = 1
+  }, 3000);
+  setTimeout(() => {
+    catHandReady.value = 2
+  }, 4950);
   wheel.value.addEventListener('transitionend', wheelStop);
 }
 // 轉動結束
 const winItemIndex = ref(0);
 const wheelStop = () => {
+  catHandSet.value = true
   const segmentAngle = 360 / list.value.length;
   const style = window.getComputedStyle(wheel.value);
   const matrix = new WebKitCSSMatrix(style.transform);
@@ -80,7 +92,7 @@ const winningItems = computed(() => {
 
 </script>
 <template>
-  <div class="tw-mx-auto tw-justify-center tw-items-center tw-flex tw-w-[100vw] tw-h-[100vh]"
+  <div class="tw-mx-auto tw-justify-center tw-items-center tw-flex tw-w-[100vw] tw-h-[100vh] tw-relative"
     :style="{
       backgroundImage: `url(src/assets/flowerBg.png)`,
       backgroundSize: 'cover',
@@ -90,9 +102,41 @@ const winningItems = computed(() => {
   >
     <div 
       class="tw-select-none tw-flex tw-justify-center tw-items-center tw-h-[40rem] tw-w-[40rem] tw-relative"
-    >
+    > 
+      <!-- 阿妹 -->
+      <div
+        class="tw-w-[26rem] tw-h-[26rem] tw-absolute tw-transition-all tw-duration-500 tw-ease-linear tw-scale-110"
+        :class="{
+          'tw-top-[-9.5rem] tw-left-[1rem] tw-opacity-100 tw-scale-125' : showAmei ,
+          'tw-top-24 tw-left-0 tw-opacity-50 tw-scale-110' : !showAmei
+        }"
+      >
+        <img src="src/assets/ameiHead.png" class="tw-w-full tw-absolute" alt="cat">
+      </div>
+      <!-- 貓掌 -->
+      <div
+        class="tw-w-[7rem] tw-h-[6rem] tw-absolute tw-transition-all tw-duration-500 tw-ease-linear tw-rotate-[-15deg] tw-scale-110"
+        :class="{
+          'tw-top-12 tw-left-[17.5rem] tw-opacity-50' : catHandReady === 0,
+          'tw-top-[-7rem] tw-left-[17.5rem] tw-opacity-100' : catHandReady === 1 ,
+          'tw-top-[1rem] tw-left-[16.5rem] tw-opacity-100' : catHandReady === 2 ,
+        }"
+      >
+        <img src="src/assets/catHandOne.png" class="tw-w-full tw-absolute" alt="cat">
+      </div>
+      <!-- 貓手 -->
+      <div
+        class="tw-w-[7rem] tw-h-[6rem] tw-absolute tw-transition-all tw-duration-500 tw-ease-linear tw-rotate-[10deg] tw-z-10 tw-scale-100 tw-top-[-1rem] tw-left-[18rem] "
+        :class="{
+          'tw-opacity-100' : catHandSet ,
+          'tw-opacity-0' : !catHandSet
+        }"
+      >
+        <img src="src/assets/catHandTwo.png" class="tw-w-full tw-absolute" alt="cat">
+      </div>
+      <!--  -->
       <div id="wheel" ref="wheel"
-        class="tw-overflow-hidden tw-rounded-full tw-absolute tw-w-[40rem] tw-h-[40rem] tw-left-0 tw-top-0 tw-flex tw-justify-center tw-items-center tw-border-4 tw-border-rose-200"
+        class="tw-overflow-hidden tw-rounded-full tw-absolute tw-w-[40rem] tw-h-[40rem] tw-left-0 tw-top-0 tw-flex tw-justify-center tw-items-center tw-border-4 tw-border-rose-200 tw-bg-white tw-mt-12"
       >
         <!-- 分割線 -->
         <div v-for="index in 10" :key="index"
@@ -118,7 +162,7 @@ const winningItems = computed(() => {
           class="tw-absolute" :class="{'tw-z-40': i.isWin}"
           :style="textPosition(index,i.isWin)"
         >
-          <span class="tw-font-bold tw-text-4xl tw-text-gray-700 tw-transition-all tw-duration-500 tw-ease-in-out"
+          <span class="tw-font-bold tw-text-4xl tw-text-gray-700 tw-transition-all tw-duration-500"
             :class="{'tw-text-8xl tw-text-orange-500': i.isWin}"
           >
             {{i.title}}
@@ -126,7 +170,7 @@ const winningItems = computed(() => {
         </div>
       </div>
       <div @click="start()" id="start" 
-        class="tw-absolute tw-cursor-pointer tw-border-gray-400 tw-w-20 tw-z-40 tw-top-[40%]"
+        class="tw-absolute tw-cursor-pointer tw-border-gray-400 tw-w-20 tw-z-40 tw-top-[50%]"
       >
         <img class="tw-w-full tw-h-full" src="src/assets/arrow.png" alt="arrow">
       </div>
