373eb7c66b703ef5404b7cecb45b58ec68918f46
Roy <roy@gastom.com.tw>
2024-02-05 18:30:45 +0800

    update route

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,7 @@
 
 const routes = [
   {
-    path: '/wedding',
+    path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
